a6bc7104d39c168f7be5f9013c3a723cae5d4b84
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-03-24 14:23:04 +0200

    Do executable check for pre/post scripts

--- files/client/ipa_01.entrypoint.sh
+++ files/client/ipa_01.entrypoint.sh
@@ -9,12 +9,12 @@ for required_env_var in IPA_01_SERVER_HOSTNAME IPA_01_DOMAIN IPA_01_ADMIN_PASSWO
 done;
 
 for file in /ipa_01.conf.d/pre/*.sh; do
-    bash "${file}";
+    [[ -x "${file}" ]] && bash "${file}";
 done;
 
 # shellcheck disable=SC2086 # Client flags are intentionally without quotes.
 ipa-client-install --unattended --server "${IPA_01_SERVER_HOSTNAME}" --domain "${IPA_01_DOMAIN,,}" --principal "admin@${IPA_01_DOMAIN^^}" --password "${IPA_01_ADMIN_PASSWORD}" ${IPA_01_CLIENT_FLAGS};
 
 for file in /ipa_01.conf.d/post/*.sh; do
-    bash "${file}";
+    [[ -x "${file}" ]] && bash "${file}";
 done;
