6072c0a27d072eca4b24b819941a705744ceaaa2
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-03-24 13:07:18 +0200

    Initial skeleton

---
+++ files/client/ipa_01.entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+set -e;
+IPA_CLIENT_FLAGS="${IPA_CLIENT_FLAGS:---no-ntp --force-join}";
+for required_env_var in IPA_01_SERVER_HOSTNAME IPA_01_DOMAIN IPA_01_ADMIN_PASSWORD; do
+    if [ "${!required_env_var}" == "" ]; then
+        echo "Failed to find value for '${required_env_var}', failing";
+        exit 1;
+    fi;
+done;
+
+for file in /ipa_01.conf.d/pre/*.sh; do
+    bash "${file}";
+done;
+
+# shellcheck disable=SC2086 # Client flags are intentionally without quotes.
+ipa-client-install --unattended --server "${IPA_01_SERVER_HOSTNAME}" --domain "${IPA_01_DOMAIN,,}" --principal "admin@${IPA_01_DOMAIN^^}" --password "${IPA_01_ADMIN_PASSWORD}" ${IPA_01_CLIENT_FLAGS};
+
+for file in /ipa_01.conf.d/post/*.sh; do
+    bash "${file}";
+done;
