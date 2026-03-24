5081dbc68ea8042f52f234450e8a043235be645c
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-03-24 14:47:26 +0200

    Fixes

--- files/server/ipa_01.entrypoint.sh
+++ files/server/ipa_01.entrypoint.sh
@@ -6,7 +6,7 @@ ipa_01_poweroff() {
 
 trap 'ipa_01_poweroff' ERR;
 
-export IPA_SERVER_FLAGS="${IPA_SERVER_FLAGS:---auto-reverse --setup-dns --no-forwarders --no-ntp}";
+IPA_SERVER_FLAGS="${IPA_SERVER_FLAGS:---auto-reverse --setup-dns --no-forwarders --no-ntp}";
 for required_env_var in IPA_01_DOMAIN IPA_01_MANAGER_PASSWORD IPA_01_ADMIN_PASSWORD; do
     if [ "${!required_env_var}" == "" ]; then
         echo "Failed to find value for '${required_env_var}', failing";
@@ -15,12 +15,12 @@ for required_env_var in IPA_01_DOMAIN IPA_01_MANAGER_PASSWORD IPA_01_ADMIN_PASSW
 done;
 
 for file in /ipa_01.conf.d/pre/*.sh; do
-    bash "${file}";
+    [[ -x "${file}" ]] && bash "${file}";
 done;
 
 # shellcheck disable=SC2086 # Server flags are intentionally without quotes.
 ipa-server-install --unattended --domain "${IPA_01_DOMAIN,,}" --realm "${IPA_01_DOMAIN^^}" --ds-password "${IPA_01_MANAGER_PASSWORD}" --admin-password "${IPA_01_ADMIN_PASSWORD}" ${IPA_01_SERVER_FLAGS};
 
 for file in /ipa_01.conf.d/post/*.sh; do
-    bash "${file}";
+    [[ -x "${file}" ]] && bash "${file}";
 done;
