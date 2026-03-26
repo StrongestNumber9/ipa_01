9f4c157f61ca94600d656a798f96cf7186b4bf3d
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-03-26 08:57:08 +0200

    Fixes paths

--- tests/functionality/files/test-runner/entrypoint.sh
+++ tests/functionality/files/test-runner/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 echo "Waiting for success scripts to exist";
 for script in pre post; do
-    for node in ipa-server ipa-client-8 ipa-client-9; do
+    for node in ipa-server ipa-client-el8 ipa-client-el9; do
         FILENAME="/return/${node}.${script}";
         echo "Waiting for '${FILENAME}' to appear";
         while [ ! -f "${FILENAME}" ]; do
